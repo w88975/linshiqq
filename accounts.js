22c13095e8619746332ff8c615129d5f4fe26a8d
KamiSama <1457434343@qq.com>
2017-09-23 04:34:49 +0800

    update

---
+++ accounts.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        "phone": "13265557961",
+        "id": "1228984723",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; __s=cq72v0b77864ku0m2s90o20dn6; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; mc=0%2C0%2C0; wo_weixin_close_count=2; __t=ut59c563a688bfb0.20504709; __u=211900852; __c=4737ea69292ef8603521d9218e43f33bd5cdd5f5; __n=%E5%B0%8F%E7%99%BE%E5%A7%9317092002820; mui=http%3A%2F%2Fimg5.baixing.net%2Fd38adabbeab5085c80e2bf30e6be3978.png_sqwbp; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3Ftoken%3D68096862ea6d2bb8bc6e447e4738b125; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; __sense_session_pv=45"
+    },
+    {
+        "phone": "13147083263",
+        "id": "1228980161",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; __s=cq72v0b77864ku0m2s90o20dn6; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; mc=0%2C0%2C0; wo_weixin_close_count=2; __t=ut59c560a76a85e6.58604626; __u=209913114; __c=a0e323fdf7950946b8f905caad481175f4f227cc; __n=%E5%B0%8F%E7%99%BE%E5%A7%93170912114198; mui=http%3A%2F%2Fimg4.baixing.net%2Ff11cf0277471a9837d6b9d6f688fd6f3.png_sqwbp; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3Ftoken%3D68096862ea6d2bb8bc6e447e4738b125; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; __sense_session_pv=34"
+    },
+    {
+        "phone": "13558886974",
+        "id": "1193027435",
+        "cookie": "__trackId=150342973042270; __city=nanning; __uuid=115034297307751.68e9a; Hm_lvt_5a727f1b4acc5725516637e03b07d3d2=1503429732; __t=ut599c400e629d69.92598327; __u=207062013; __c=711abb3e33431994039b9f48ef1ff0523281345a; __n=KamiSama639; _ga=GA1.2.164676344.1503430108; _gid=GA1.2.290939878.1503430108; Hm_lpvt_5a727f1b4acc5725516637e03b07d3d2=1503484153; mui=http%3A%2F%2Fimg4.baixing.net%2Ff2808ecaebf645e9aa3e1be464fccfd2.png_sq; mc=8%2C0%2C0; __chat_udid=00ef75be-59b4-4d22-98a6-500cadbfdace; verified_device_id=dddde5d94fb84a631f83304efc0576aa321f2cb5643424bb; _auth_redirect=http%3A%2F%2Fchengdu.baixing.com%2Ffabu%2F1193027435%3Ftoken%3D2f3289f7ad758dc34c38ed676601abd3; __sense_session_pv=1; _gat=1"
+    },
+
+    {
+        "phone": "13148883686",
+        "id": "1229010874",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; appId=8brlm7ufr6863; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; u207062013_f5a298f18e576u207062013_f5a298f18e576=1506009600000; mc=0%2C0%2C0; u212503275_8f9dc8a11d84eu212503275_8f9dc8a11d84e=1506009600000; wo_weixin_close_count=2; u209913114_be31525fc5690u209913114_be31525fc5690=1506009600000; u209913114_be31525fc5690=1506108085275; u211900852_3e03c2d278092=1506108998909; u211900852_3e03c2d278092u211900852_3e03c2d278092=1506108998909; __t=ut59c570845ea757.66046132; __u=211900703; __c=7fceaf975d6194eb36d864d3109a1f5ef122c644; __n=%E5%B0%8F%E7%99%BE%E5%A7%9317092002726; mui=http%3A%2F%2Fimg5.baixing.net%2Faf467dcdbf13adc6924f734609387bf0.png_sqwbp; u211900703_dbae428de9f7f=1505841231197; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3F; navi2ac9b344=120.92.13.121:80,u211900703_dbae428de9f7f; rongSDK=websocket; u211900703_dbae428de9f7fu211900703_dbae428de9f7f=1506009600000; __sense_session_pv=15; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE"
+    },
+
+    {
+        "phone": "13426184328",
+        "id": "1229012790",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; __s=cq72v0b77864ku0m2s90o20dn6; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; wo_weixin_close_count=2; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; mc=171%2C0%2C0; __t=ut59c5717db14e45.17754689; __u=208188783; __c=e5ad711f6d09da2e932d2c352466209a34c41f21; __n=%E5%B0%8F%E7%99%BE%E5%A7%9317090307128; mui=http%3A%2F%2Fimg4.baixing.net%2F60147943e464faef76712e98222f2f72.png_sqwbp; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3F; __sense_session_pv=40"
+    },
+
+    {
+        "phone": "15818534470",
+        "id": "1229014710",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; appId=8brlm7ufr6863; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; u207062013_f5a298f18e576u207062013_f5a298f18e576=1506009600000; u212503275_8f9dc8a11d84eu212503275_8f9dc8a11d84e=1506009600000; wo_weixin_close_count=2; u209913114_be31525fc5690u209913114_be31525fc5690=1506009600000; u209913114_be31525fc5690=1506108085275; u211900852_3e03c2d278092=1506108998909; u211900852_3e03c2d278092u211900852_3e03c2d278092=1506108998909; u211900703_dbae428de9f7f=1505841231197; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; u211900703_dbae428de9f7fu211900703_dbae428de9f7f=1506009600000; mc=171%2C0%2C0; u208188783_91bd0c4fdb331u208188783_91bd0c4fdb331=1506009600000; __t=ut59c5722618b161.88440491; __u=207957765; __c=44304798f87eabef2f56f1bc874d973139c78945; __n=%E5%B0%8F%E7%99%BE%E5%A7%9317083111815; mui=http%3A%2F%2Fimg6.baixing.net%2F8293c8cb910a056fa2dc1e52f87c0a89.png_sqwbp; __sense_session_pv=49; navi37e945a9=120.92.93.249:80,u207957765_e130679d86bf5; rongSDK=websocket; u207957765_e130679d86bf5u207957765_e130679d86bf5=1506009600000"
+    },
+
+    {
+        "phone": "15652903500",
+        "id": "1229015324",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; appId=8brlm7ufr6863; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; u207062013_f5a298f18e576u207062013_f5a298f18e576=1506009600000; u212503275_8f9dc8a11d84eu212503275_8f9dc8a11d84e=1506009600000; wo_weixin_close_count=2; u209913114_be31525fc5690u209913114_be31525fc5690=1506009600000; u209913114_be31525fc5690=1506108085275; u211900852_3e03c2d278092=1506108998909; u211900852_3e03c2d278092u211900852_3e03c2d278092=1506108998909; u211900703_dbae428de9f7f=1505841231197; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; u211900703_dbae428de9f7fu211900703_dbae428de9f7f=1506009600000; mc=171%2C0%2C0; u208188783_91bd0c4fdb331u208188783_91bd0c4fdb331=1506009600000; naviacb33c31=120.92.93.249:80,u207957765_e130679d86bf5; rongSDK=websocket; u207957765_e130679d86bf5u207957765_e130679d86bf5=1506009600000; __t=ut59c572a6736097.66357411; __u=206882164; __c=01522e0220ba090fcf8c5a1cc544b6dba35216d4; __n=%E5%B0%8F%E7%99%BE%E5%A7%93170821116408; mui=http%3A%2F%2Fimg6.baixing.net%2F2029c7b9d269d78ea90b87273b324cf0.png_sqwbp; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3F; __sense_session_pv=58"
+    },
+
+    {
+        "phone": "13420437640",
+        "id": "1229017242",
+        "cookie": "__trackId=150600164820237; __city=chengdu; __uuid=115060016522626.fe1f4; login_on_tab=0; verified_device_id=794165672865e1eee195a247bfe449f429a52d1c1fc28b49; appId=8brlm7ufr6863; __chat_udid=f64bc159-cecc-4360-a45f-fd0aed9b5243; u207062013_f5a298f18e576u207062013_f5a298f18e576=1506009600000; u212503275_8f9dc8a11d84eu212503275_8f9dc8a11d84e=1506009600000; wo_weixin_close_count=2; u209913114_be31525fc5690u209913114_be31525fc5690=1506009600000; u209913114_be31525fc5690=1506108085275; u211900852_3e03c2d278092=1506108998909; u211900852_3e03c2d278092u211900852_3e03c2d278092=1506108998909; u211900703_dbae428de9f7f=1505841231197; __admx_track_id=W1dOx85VXksT_dqvAJ1nCA; __admx_track_id.sig=rnNtK7pmqGl55ttNKSb49r94nYE; u211900703_dbae428de9f7fu211900703_dbae428de9f7f=1506009600000; mc=171%2C0%2C0; u208188783_91bd0c4fdb331u208188783_91bd0c4fdb331=1506009600000; u207957765_e130679d86bf5u207957765_e130679d86bf5=1506009600000; u206882164_069b17d5d244du206882164_069b17d5d244d=1506009600000; u212503444_698693e8c89c6u212503444_698693e8c89c6=1506009600000; __t=ut59c57387e850a6.08196995; __u=212503108; __c=e396d32ca94c70a90ae1ee0026e804dcde6ba5dc; __n=%E5%B0%8F%E7%99%BE%E5%A7%9317092301536; mui=http%3A%2F%2Fimg5.baixing.net%2Fd790bd42f0a537f237d529b82b935e9b.png_sqwbp; _auth_redirect=http%3A%2F%2Fwww.baixing.com%2Ffabu%2Fpaifa%2F%3Ftoken%3D4d69d19b2fa521e247ab805aeaf340c6; __sense_session_pv=81; navic9f38461=120.92.77.224:80,u212503108_15ff18bbc6cab; rongSDK=websocket; u212503108_15ff18bbc6cabu212503108_15ff18bbc6cab=1506009600000"
+    },
+]
---
+++ app2.js
@@ -0,0 +1,158 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+const request = require('request').defaults({ jar: true });
+
+
+var accounts = require('./accounts.js')
+
+var fetch = async (url, options) => {
+    return new Promise(function (resolve, reject) {
+        request(url, options, (err, httpResponse, body) => {
+            resolve(httpResponse)
+        })
+    });
+}
+
+var Haker = async function (phone, id, qq, cookie) {
+    const jar = request.jar()
+    var cookiestr = cookie
+    var cookies = cookiestr.split(';')
+    cookies.map(item => {
+        jar.setCookie(item, 'http://chengdu.baixing.com')
+    })
+    let data = await fetch('http://chengdu.baixing.com/fabu/' + id, {
+        method: 'GET',
+        followRedirect: false,
+        jar: jar,
+    })
+    var result = data.body.match('玩转视频发布</a> ）</small>"},.*],"forms":{"tabs"')[0]
+    result = result.replace('玩转视频发布</a> ）</small>"},', '').replace('],"forms":{"tabs"', '')
+    result = '[' + result
+    result = result + ']'
+    var config = JSON.parse(result)
+
+    var token, _8cb44b44cba8fde
+    config.map(item => {
+        if (item.name === 'token') {
+            token = item.value
+        }
+        if (item.name === '8cb44b44cba8fde') {
+            _8cb44b44cba8fde = item.value
+        }
+    })
+    var querystring = 'title=%E6%89%93%E5%AD%97%E5%91%98100%E6%AF%8F%E5%A4%A9&%E5%B7%A5%E8%B5%84=100&%E5%B7%A5%E4%BD%9C%E6%97%B6%E9%97%B4=&%E6%8B%9B%E8%81%98%E4%BA%BA%E6%95%B0=&content=&%E5%85%AC%E5%8F%B8%E5%90%8D%E7%A7%B0=%E4%B8%AA%E4%BA%BA&lat=&lng=&geoCity=&%E5%9C%B0%E5%8C%BA%5B%5D=m7144&%E5%9C%B0%E5%8C%BA%5B%5D=m2971&%E5%85%B7%E4%BD%93%E5%9C%B0%E7%82%B9=%E7%8E%8B%E5%AE%B6%E5%A1%98%E8%A1%9784%E5%8F%B7&%E5%BE%AE%E4%BF%A1%E5%8F%B7=&QQ%E5%8F%B7=' + qq + '&contact=13147083263&allowChatOnly=0&video=&token=' + token + '&8cb44b44cba8fde=' + _8cb44b44cba8fde + '&%E7%B1%BB%E5%9E%8B=&%E7%BB%93%E7%AE%97%E6%96%B9%E5%BC%8F=&images%5B%5D=&file=&video-file=&shortVideoUri=&pay=&agreement=on'
+    let data2 = await fetch('http://chengdu.baixing.com/fabu/' + id + '#', {
+        method: 'POST',
+        followRedirect: false,
+        jar: jar,
+        headers: {
+            "Host": "chengdu.baixing.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": querystring.length,
+            "Referer": "http://chengdu.baixing.com/fabu/" + id,
+            "Cookie": cookiestr,
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": 1
+        },
+        body: querystring
+    })
+    console.log('data2.statusCode', data2.statusCode)
+    if (data2.statusCode === 302) {
+        let data3 = await fetch('http://chengdu.baixing.com/fabu/success?adId=' + id + '&category=wangluojianzhi&city=chengdu', {
+            method: 'GET',
+            followRedirect: false,
+            jar: jar,
+            headers: {
+                "Host": "chengdu.baixing.com",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+                "Accept-Encoding": "gzip, deflate",
+                "Connection": "keep-alive",
+                "Pragma": "no-cache",
+                "Cache-Control": "no-cache"
+            }
+        })
+        console.log('data3.statusCode', data3.statusCode)
+        if (data3.statusCode === 200) {
+            let data4 = await fetch('http://www.baixing.com/oz/login/?redirect=http://chengdu.baixing.com/fabu/success?adId=' + id + '&category=wangluojianzhi&city=chengdu', {
+                method: 'GET',
+                followRedirect: false,
+                jar: jar,
+                headers: {
+                    "Host": "www.baixing.com",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+                    "Accept-Encoding": "gzip, deflate",
+                    "Connection": "keep-alive",
+                    "Pragma": "no-cache",
+                    "Cache-Control": "no-cache"
+                }
+            })
+            console.log('data4.statusCode', data4.statusCode)
+            if (data4.statusCode === 200) {
+                console.log('成功')
+                return true
+            }
+        }
+        return false
+    }
+    return false
+}
+
+//'80515552'
+var HakerMap = async function (qq) {
+    for (var i = 0; i < accounts.length; ++i) {
+        console.log(`正在用: ${accounts[i].phone} 操作QQ: ${qq}`)
+        let result = await Haker(accounts[i].phone, accounts[i].id, qq, accounts[i].cookie)
+        if (result) {
+            return {
+                cg: true,
+                qq,
+                msg: '成功'
+            }
+        }
+    }
+
+    return {
+        cg: false,
+        qq,
+        msg: '失败'
+    }
+}
+
+
+// HakerMap('80515553')
+
+var sleep = function (time) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, time);
+    })
+};
+
+
+app.post('/qq', async (req, res) => {
+    var qqs = req.body.qq.split('\r\n')
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });//设置response编码为utf-8
+    for (var index = 0; index < qqs.length; index++) {
+        let result = await HakerMap(qqs[index])
+        res.write(`<div>QQ: [${qqs[index]}] ${result.msg} </div><br/>`)
+        // res.write(`<div>等待5秒</div>`)
+        await sleep(1000)
+    }
+    res.end()
+})
+
+app.get('/', async (req, res) => {
+    res.sendFile(process.cwd() + '/index.html')
+})
+app.listen(4444)
